573c75e3bc9fb9dce61a88d664ebcb0499f2d099
Zhu Yue <zhuyue01@baidu.com>
2016-02-12 15:47:03 +0800

    Project initialized.

---
+++ XMPPDemo/XMPPDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPDemo
+//
+//  Created by zhuyue on 16/2/12.
+//  Copyright © 2016年 zhuyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPDemo/XMPPDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMPPDemo
+//
+//  Created by zhuyue on 16/2/12.
+//  Copyright © 2016年 zhuyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
